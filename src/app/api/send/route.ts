20b9cf45e5922d527729059c777f6e10fdc14408
Vlas-pravsha <vlas20421@gmail.com>
2023-10-25 23:11:42 +0300

    Commented route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,18 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { Resend } from "resend";
+export{}
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { Resend } from "resend";
 
-const resendClient = new Resend("re_iGCcmofu_81TuR37etPgbn2J5JyR4yd8a");
+// const resendClient = new Resend("re_iGCcmofu_81TuR37etPgbn2J5JyR4yd8a");
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const data = await resendClient.emails.send({
-      from: "onboarding@resend.dev",
-      to: "vlas20421@gmail.com",
-      subject: "Hello World",
-      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-    });
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+// interface EmailData {
+//   from: string;
+//   to: string;
+//   subject: string;
+//   html: string;
+// }
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const emailData: EmailData = {
+//       from: "onboarding@resend.dev",
+//       to: "vlas20421@gmail.com",
+//       subject: "Hello World",
+//       html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+//     };
+
+//     const data = await resendClient.emails.send(emailData);
+//     res.status(200).json(data);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
